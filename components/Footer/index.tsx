1538a8757df31f461ec60a31f3f7393a16e32ac9
Hyorran <hyorran.preigschadt.contractor@euronext.com>
2024-10-04 16:21:12 +0100

    removed unused files

--- components/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./_index.scss"
-
-const Footer = () => {
-  return <footer className="footer-container">&#169;</footer>
-}
-
-export { Footer }
--- components/index.tsx
+++ components/index.tsx
@@ -1,10 +1,8 @@
 import { Navbar } from "./Navbar"
-import { Footer } from "./Footer"
 import { Layout } from "./Layout"
 import Table from "./Table"
-import AccordionItem from "./AccordionItem"
 import Form from "./Form"
 import Card from "./Card"
 import Comment from "./Comment"
 
-export { Navbar, Footer, Layout, Table, AccordionItem, Form, Card, Comment }
+export { Navbar, Layout, Table, Form, Card, Comment }
